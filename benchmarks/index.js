c5ccfeac45f25bec62119a3048fad98039b2bb43
Michał Sieczkowski <michal@ethworks.io>
2020-05-19 13:16:37 +0200

    Run many benchmark samples and calculate metrics

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -1,3 +1,5 @@
+const { average, std } = require("./utils/metrics")
+const { formatNanos, formatMs } = require("./utils/formatTime")
 const { run: run01 } = require('./01-eth-transfers')
 const { run: run02 } = require('./02-erc20-transfers')
 const { run: run03 } = require('./03-erc20-deploys')
@@ -5,29 +7,42 @@ const { run: run04 } = require('./04-erc20-calls')
 const { run: run05 } = require('./05-erc20-storage')
 const { run: run06 } = require('./06-many-storage')
 
-const repeat = 1
-const samples = 20
+const samples = 10
 const scale = 0.01
+const logSampleTimes = true
 
-async function benchmark (name, fn, runs) {
-  console.time(name)
-  await fn(Math.ceil(runs))
-  console.timeEnd(name)
+function log(name, runs, msg) {
+  console.log(`${name} | samples: ${samples}, runs: ${runs} | ${msg}`);
 }
 
-// NOTE: the runs value is experimentally determined to cause 10 seconds of execution
-async function main () {
-  for (let i = 0; i < repeat; i++) {
-    await benchmark('01 |   eth-transfers', run01, scale * 300)
-    await benchmark('02 | erc20-transfers', run02, scale * 200)
-    await benchmark('03 |   erc20-deploys', run03, scale * 125)
-    await benchmark('04 |     erc20-calls', run04, scale * 1200)
-    await benchmark('05 |   erc20-storage', run05, scale * 220)
-    await benchmark('06 |    many-storage', run06, scale * 12)
-    if (repeat > 1) {
-      console.log('Finished run', i + 1)
+async function benchmark(name, fn, runsFrac) {
+  const runs = Math.ceil(runsFrac)
+  const sampleTimes = new Array(samples)
+
+  // warm-up
+  await fn(runs)
+  await fn(runs)
+
+  for (let i = 0; i < samples; i++) {
+    sampleTimes[i] = await fn(runs)
+    if (logSampleTimes) {
+      log(name, runs, `#${i + 1} sample time: ${formatNanos(sampleTimes[i])}`)
     }
   }
+
+  const avgTime = average(sampleTimes)
+  const timesStd = std(sampleTimes)
+
+  log(name, runs, `average time: ${formatNanos(avgTime)}, std: ${formatMs(timesStd)}`)
+}
+
+async function main() {
+  await benchmark('01 |   eth-transfers', run01, scale * 300)
+  await benchmark('02 | erc20-transfers', run02, scale * 200)
+  await benchmark('03 |   erc20-deploys', run03, scale * 125)
+  await benchmark('04 |     erc20-calls', run04, scale * 1200)
+  await benchmark('05 |   erc20-storage', run05, scale * 220)
+  await benchmark('06 |    many-storage', run06, scale * 12)
 }
 
 main()
--- benchmarks/utils/formatTime.js
+++ benchmarks/utils/formatTime.js
@@ -1,10 +1,23 @@
 const prettyTime = require('pretty-time')
+
 /**
  * Get pretty string representation of nanoseconds
  * @param nanoseconds BigInt timestamp
  */
-exports.formatTime = function (nanoseconds) {
+function formatNanos(nanoseconds) {
   const seconds = nanoseconds / BigInt(Math.pow(10, 9))
   const remainingNs = nanoseconds - seconds * BigInt(Math.pow(10, 9))
   return prettyTime([Number(seconds), Number(remainingNs)], 'ms')
+}
+
+function formatMs(milliseconds) {
+  const seconds = milliseconds / Math.pow(10, 3)
+  const remainingMs = milliseconds - seconds * Math.pow(10, 3)
+  const remainingNs = remainingMs * Math.pow(10, 6)
+  return prettyTime([seconds, remainingNs], 'ms')
+}
+
+module.exports = {
+  formatNanos,
+  formatMs
 }
