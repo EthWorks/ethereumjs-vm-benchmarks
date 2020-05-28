128ffdd07e9a5743bcc7d4f22ea62374f74464f2
Michał Sieczkowski <michal@ethworks.io>
2020-05-28 12:21:10 +0200

    Create tests making ERC20 balanceOf calls on raw VM

---
+++ test/benchmarks/04-erc20-calls.test.ts
@@ -0,0 +1,57 @@
+import { expect } from 'chai'
+import { Wallet } from 'ethers'
+import { Contract } from 'ethers/contract'
+import { parseEther } from 'ethers/utils'
+import { SimpleProvider } from '../../src/benchmarks/SimpleProvider'
+import { createSimpleChain, SimpleChain } from '../../src/benchmarks/SimpleChain'
+import { deployERC20 } from './utils/deploy'
+import { getERC20BalanceOfCall } from './utils/calls'
+import { getERC20TransferTransaction } from './utils/transactions'
+
+describe('ERC20 calls', () => {
+  let chain: SimpleChain
+  let provider: SimpleProvider
+  let deployer: Wallet
+  let token: Contract
+
+  beforeEach(async () => {
+    chain = await createSimpleChain()
+    provider = new SimpleProvider(chain);
+    ([deployer] = provider.getWallets())
+    token = await deployERC20(deployer, chain)
+  })
+
+  it('supports balanceOf calls', async () => {
+    const other = provider.createEmptyWallet()
+    const empty = provider.createEmptyWallet()
+
+    const transferParams = {
+      tokenAddress: token.address,
+      from: deployer,
+      to: other,
+      value: parseEther('1'),
+    }
+    const transfer = await getERC20TransferTransaction(transferParams)
+    await chain.sendTransaction(transfer)
+
+    const balanceOfDeployer = await getERC20BalanceOfCall(token.address, deployer.address)
+    const hexDeployerBalance = await chain.call(balanceOfDeployer, 'latest')
+    expect(parseEther('99').eq(hexDeployerBalance)).to.be.true
+
+    const balanceOfOther = await getERC20BalanceOfCall(token.address, other.address)
+    const hexOtherBalance = await chain.call(balanceOfOther, 'latest')
+    expect(parseEther('1').eq(hexOtherBalance)).to.be.true
+
+    const balanceOfEmpty = await getERC20BalanceOfCall(token.address, empty.address)
+    const hexEmptyBalance = await chain.call(balanceOfEmpty, 'latest')
+    expect(parseEther('0').eq(hexEmptyBalance)).to.be.true
+  })
+
+  it('supports multiple balanceOf calls', async () => {
+    for (let i = 0; i < 10; i++) {
+      const balanceOf = await getERC20BalanceOfCall(token.address, deployer.address)
+      const hexBalance = await chain.call(balanceOf, 'latest')
+      expect(parseEther('100').eq(hexBalance)).to.be.true
+    }
+  })
+})
---
+++ test/benchmarks/utils/calls.ts
@@ -0,0 +1,12 @@
+import { Interface } from "ethers/utils"
+import { toRpcTransactionRequest } from '../../../src/model'
+const ERC20Mock = require('../../../contracts/ERC20Mock.json')
+
+export async function getERC20BalanceOfCall (tokenAddress: string, accountAddress: string) {
+  const erc20Interface = new Interface(ERC20Mock.abi)
+  const balanceOfCall = {
+    data: erc20Interface.functions.balanceOf.encode([accountAddress]),
+    to: tokenAddress,
+  }
+  return toRpcTransactionRequest(balanceOfCall)
+}
