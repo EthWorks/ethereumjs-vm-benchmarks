40fd6a0bd01bffffbbd4beb8a0e99ada03b835c9
Michał Sieczkowski <michal@ethworks.io>
2020-06-01 23:22:43 +0200

    Make 04-erc20-calls benchmark run on SimpleChain

--- src/benchmarks/04-erc20-calls.ts
+++ src/benchmarks/04-erc20-calls.ts
@@ -1,30 +1,48 @@
 import { SimpleChain, SimpleProvider } from '../chain'
-import { ContractFactory, utils } from 'ethers'
 import { measureExecution } from './utils/measureExecution'
 import { choose1 } from './utils/random'
-
-const ERC20Mock = require('../../contracts/ERC20Mock.json')
+import { createNonceCounter } from './utils/NonceCounter'
+import { getERC20DeployTransaction, getERC20TransferTransaction } from './utils/transactions'
+import { parseEther } from 'ethers/utils'
+import { getERC20BalanceOfCall } from './utils/calls'
 
 export async function run (runs: number, chain: SimpleChain) {
   const provider = new SimpleProvider(chain)
   const wallets = provider.getWallets()
-  const erc20Factory = new ContractFactory(ERC20Mock.abi, ERC20Mock.bytecode, wallets[0])
-  const token = await erc20Factory.deploy(wallets[0].address, utils.parseEther('1000000'))
+  const nonceCounter = await createNonceCounter(wallets)
 
+  // Deploy ERC20
+  const deployParams = {
+    deployer: wallets[0],
+    initialAccount: wallets[0].address,
+    initialBalance: parseEther('1000000'),
+    nonceCounter,
+  }
+  const { deployment, futureAddress: tokenAddress } = await getERC20DeployTransaction(deployParams)
+  await chain.sendTransaction(deployment)
+
+  // Supply each remaining wallet with initial token balance
   for (let i = 1; i < wallets.length; i++) {
-    await token.transfer(wallets[i].address, utils.parseEther('10000'))
+    const transferParams = {
+      tokenAddress,
+      from: wallets[0],
+      to: wallets[i],
+      value: parseEther('10000'),
+      nonceCounter,
+    }
+    const initialTransfer = await getERC20TransferTransaction(transferParams)
+    await chain.sendTransaction(initialTransfer)
   }
 
-  const data = new Array(runs)
-
+  const balanceOfCalls = new Array(runs)
   for (let i = 0; i < runs; i++) {
-    data[i] = choose1(wallets)
+    const wallet = choose1(wallets)
+    balanceOfCalls[i] = getERC20BalanceOfCall(tokenAddress, wallet.address)
   }
 
   return measureExecution(async function () {
     for (let i = 0; i < runs; i++) {
-      const holder = data[i]
-      await token.balanceOf(holder.address)
+      await chain.call(balanceOfCalls[i], 'latest')
     }
   })
 }
