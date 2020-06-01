5d6f820d91f003c0cc02973e2b9ad09f38db688a
Michał Sieczkowski <michal@ethworks.io>
2020-06-02 00:06:53 +0200

    Make 06-many-storage benchmark run on SimpleChain

--- src/benchmarks/06-many-storage.ts
+++ src/benchmarks/06-many-storage.ts
@@ -1,9 +1,13 @@
-import { ContractFactory, utils } from 'ethers'
+import { utils } from 'ethers'
 import { measureExecution } from './utils/measureExecution'
-import { choose1, randomEthAddress, randomEthValue } from './utils/random'
+import { choose1, randomEthValue } from './utils/random'
 import { SimpleChain, SimpleProvider } from '../chain'
-
-const ERC20Mock = require('../../contracts/ERC20Mock.json')
+import { createNonceCounter } from './utils/NonceCounter'
+import {
+  getERC20ApproveTransaction,
+  getERC20DeployTransaction,
+  getERC20TransferTransaction,
+} from './utils/transactions'
 
 const numApprovals = 10
 const oneMillionEth = utils.parseEther('1000000')
@@ -12,33 +16,61 @@ const tenThousandEth = utils.parseEther('10000')
 export async function run (runs: number, chain: SimpleChain) {
   const provider = new SimpleProvider(chain)
   const wallets = provider.getWallets()
-  const erc20Factory = new ContractFactory(ERC20Mock.abi, ERC20Mock.bytecode, wallets[0])
+  const nonceCounter = await createNonceCounter(wallets)
 
-  const data = new Array(runs)
+  // Prepare data for benchmark execution
+  const signedTransactions = new Array(runs)
 
   for (let i = 0; i < runs; i++) {
-    data[i] = new Array(numApprovals)
+    signedTransactions[i] = {}
 
-    for (let j = 0; j < numApprovals; j++) {
-      data[i][j] = {
-        holder: choose1(wallets),
-        spender: randomEthAddress(),
-        value: randomEthValue(10, 50),
+    // ERC20 deployment transaction
+    const deployParams = {
+      deployer: wallets[0],
+      initialAccount: wallets[0].address,
+      initialBalance: oneMillionEth,
+      nonceCounter,
+    }
+    const erc20 = await getERC20DeployTransaction(deployParams)
+    signedTransactions[i].deployment = erc20.deployment
+
+    // ERC20 transfer transactions
+    signedTransactions[i].transfers = new Array(wallets.length - 1)
+    for (let j = 0; j < wallets.length - 1; j++) {
+      const transferParams = {
+        tokenAddress: erc20.futureAddress,
+        from: wallets[0],
+        to: wallets[j + 1],
+        value: tenThousandEth,
+        nonceCounter,
       }
+      signedTransactions[i].transfers[j] = await getERC20TransferTransaction(transferParams)
+    }
+
+    // ERC20 approve transactions
+    signedTransactions[i].approves = new Array(numApprovals)
+    for (let j = 0; j < numApprovals; j++) {
+      const approveParams = {
+        tokenAddress: erc20.futureAddress,
+        owner: choose1(wallets),
+        spender: provider.createEmptyWallet(),
+        amount: randomEthValue(10, 50),
+        nonceCounter,
+      }
+      signedTransactions[i].approves[j] = await getERC20ApproveTransaction(approveParams)
     }
   }
 
   return measureExecution(async function () {
     for (let i = 0; i < runs; i++) {
-      const token = await erc20Factory.deploy(wallets[0].address, oneMillionEth)
+      await chain.sendTransaction(signedTransactions[i].deployment)
 
-      for (let j = 1; j < wallets.length; j++) {
-        await token.transfer(wallets[j].address, tenThousandEth)
+      for (let j = 0; j < wallets.length - 1; j++) {
+        await chain.sendTransaction(signedTransactions[i].transfers[j])
       }
 
       for (let j = 0; j < numApprovals; j++) {
-        const { holder, spender, value } = data[i][j]
-        await token.connect(holder).approve(spender, value)
+        await chain.sendTransaction(signedTransactions[i].approves[j])
       }
     }
   })
