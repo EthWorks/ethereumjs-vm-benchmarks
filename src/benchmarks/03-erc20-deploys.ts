bf4ae3f96d6296ff83c1976da233f69d1092677b
Michał Sieczkowski <michal@ethworks.io>
2020-06-01 20:35:23 +0200

    Make 03-erc20-deploys benchmark run on SimpleChain

--- src/benchmarks/03-erc20-deploys.ts
+++ src/benchmarks/03-erc20-deploys.ts
@@ -1,31 +1,32 @@
-import { ContractFactory } from 'ethers'
 import { measureExecution } from './utils/measureExecution'
 import { choose1, randomEthValue } from './utils/random'
 import { SimpleChain, SimpleProvider } from '../chain'
-
-const ERC20Mock = require('../../contracts/ERC20Mock.json')
+import { createNonceCounter } from './utils/NonceCounter'
+import { getERC20DeployTransaction } from './utils/transactions'
 
 export async function run (runs: number, chain: SimpleChain) {
   const provider = new SimpleProvider(chain)
   const wallets = provider.getWallets()
-  const erc20Factory = new ContractFactory(ERC20Mock.abi, ERC20Mock.bytecode, wallets[0])
-  const data = new Array(runs)
+  const nonceCounter = await createNonceCounter(wallets)
 
+  // Prepare data for benchmark execution
+  const signedDeploys = new Array(runs)
   for (let i = 0; i < runs; i++) {
     const wallet = choose1(wallets)
-    const factory = erc20Factory.connect(wallet)
     const initialBalance = randomEthValue(40, 500)
-    data[i] = {
-      wallet,
-      factory,
+    const deployParams = {
+      deployer: wallet,
+      initialAccount: wallet.address,
       initialBalance,
+      nonceCounter,
     }
+    const erc20 = await getERC20DeployTransaction(deployParams)
+    signedDeploys[i] = erc20.deployment
   }
 
   return measureExecution(async function () {
     for (let i = 0; i < runs; i++) {
-      const { wallet, factory, initialBalance } = data[i]
-      await factory.deploy(wallet.address, initialBalance)
+      await chain.sendTransaction(signedDeploys[i])
     }
   })
 }
